9664bcea7daba5d135a4a8a95af8f8ddfae3a819
Alison Debaille <adebaille@gmail.com>
2026-07-07 22:59:13 +0200

    exercise 3, not done

--- récursivité/script.js
+++ récursivité/script.js
@@ -15,3 +15,17 @@ function countdown(n) {
     countdown(n - 1);
 }
 countdown(3);
+
+//Exercice 3
+//Écris une fonction récursive flatten(arr) qui aplatit un tableau imbriqué, quel que soit le niveau d'imbrication.
+// solution donnée (je n'ai pas réussi à le faire seule)
+function flatten(arr) {
+  return arr.reduce((acc, element) => {
+    if (Array.isArray(element)) {
+      return [...acc, ...flatten(element)];
+    } else {
+      return [...acc, element];
+    }
+  }, []);
+}
+flatten([1, [2, [3, [4]]], 5]); // [1, 2, 3, 4, 5]
