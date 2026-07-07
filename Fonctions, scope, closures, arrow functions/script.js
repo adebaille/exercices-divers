c7167d0994a9d2a8eaafe74fa5ee405611ddaa2a
Alison Debaille <adebaille@gmail.com>
2026-07-07 17:04:13 +0200

    exercise 1 & 2 done

---
+++ Fonctions, scope, closures, arrow functions/script.js
@@ -0,0 +1,12 @@
+//Exercice 1
+//Écris une arrow function multiply qui prend deux nombres et retourne leur produit. Utilise la syntaxe courte (sans accolades ni return explicite).
+const multiply = (a, b) => a * b;
+
+//Exercice 2
+// Écris une fonction greet qui prend un name et retourne "Bonjour, [name] !". Appelle-la avec ton prénom.
+function greet(name) {
+
+    return `Bonjour, ${name} !`;
+}
+
+console.log(greet("Alison"));
