0f6b3fe9b4860b6848ed82be2b2f4280f3c4a759
Alison Debaille <adebaille@gmail.com>
2026-07-07 17:19:56 +0200

    exercice 4 done

--- Fonctions, scope, closures, arrow functions/script.js
+++ Fonctions, scope, closures, arrow functions/script.js
@@ -24,3 +24,18 @@ console.log(message);
 
 /* Réponse : La variable message à l'intérieur de foo "masque" la variable globale du même nom, uniquement dans le scope de la fonction. Les deux coexistent sans se modifier.
 JS cherche toujours une variable du scope le plus proche vers le plus lointain. Dans foo, il trouve message localement, il s'arrête là et n'ira pas chercher le message global. */
+
+//Exercice 4 
+// Écris une fonction makeMultiplier(factor) qui retourne une nouvelle fonction. Cette nouvelle fonction prend un nombre et le multiplie par factor.
+function makeMultiplier(factor) {
+
+  return function(n) {
+    return factor * n;
+  };
+}
+
+const double = makeMultiplier(2);
+const triple = makeMultiplier(3);
+
+console.log(double(5));  // 10
+console.log(triple(5));  // 15
