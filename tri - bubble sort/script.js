f83185f9768d6e637dcaada4fa9670c28334e328
Alison Debaille <adebaille@gmail.com>
2026-07-08 12:51:53 +0200

    exercice 2 done

--- tri - bubble sort/script.js
+++ tri - bubble sort/script.js
@@ -20,3 +20,15 @@ Tour 3 :
 1 > 2 → échange → [1, 2, 4, 3, 7, 9]
 4 > 2 ? → rien  → [1, 2, 4, 3, 7, 9]
 4 > 3 → échange → [1, 2, 3, 4, 7, 9] Trié */
+
+//Exercice 2
+//Écris une fonction linearSearch qui cherche une valeur dans un tableau et retourne son index, ou -1 si elle n'est pas trouvée. Teste avec
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
+}
+
+linearSearch([10, 25, 8, 42, 16], 42) // 3
+linearSearch([10, 25, 8, 42, 16], 99) // -1
